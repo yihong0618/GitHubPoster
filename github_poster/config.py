5a05418293019f0ccfcd9c8848b4aa61d17f6f65
yihong0618 <zouzou0208@gmail.com>
2021-07-13 08:41:05 +0800

    fix: #27

---
+++ github_poster/config.py
@@ -0,0 +1,16 @@
+DOM_TUPLE = (2.6, 2.6)
+DEFAULT_DOM_COLOR = "#444444"
+MONTH_NAMES = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+]
