382a1e0a1e0eb9a0f7fed0866e03a2530568eed8
umm233 <umm233@protonmail.com>
2021-12-23 23:21:30 +0800

    move some method to html parse

--- github_poster/loader/jike_loader.py
+++ github_poster/loader/jike_loader.py
@@ -1,12 +1,16 @@
 import json
-import random
-import time
 from http.cookies import SimpleCookie
-from re import compile, findall
+from re import compile
 
 import pendulum
 import requests
 
+from github_poster.html_parser.jike_parse import (
+    find_date_in_response,
+    find_dateTime_in_html,
+    find_last_id_in_html,
+    find_last_id_in_response,
+)
 from github_poster.loader.base_loader import BaseLoader, LoadError
 from github_poster.loader.config import JIKE_GRAPHQL_URL, JIKE_PERSON_URL
 
@@ -22,12 +26,10 @@ class JikeLoader(BaseLoader):
         self.session = requests.Session()
         self.headers = {
             "authority": "web-api.okjike.com",
-            "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
             "accept": "*/*",
             "dnt": "1",
             "content-type": "application/json",
             "sec-ch-ua-mobile": "?0",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
             "sec-ch-ua-platform": '"Windows"',
             "origin": "https://web.okjike.com",
             "sec-fetch-site": "same-site",
@@ -65,39 +67,6 @@ class JikeLoader(BaseLoader):
             )
         return cookiejar
 
-    def _get_response_last_id(self, post_resp_json):
-        """
-        get last id from response
-        """
-        json_data = json.loads(post_resp_json)
-        load_more_key = json_data["data"]["userProfile"]["feeds"]["pageInfo"][
-            "loadMoreKey"
-        ]
-        return "" if load_more_key == None else load_more_key["lastId"]
-
-    def _find_html_last_id(self, text):
-        """
-        find last id in html
-        "json":{"lastId":"xxx"}}
-        """
-        r = findall('"json":{"lastId":"(.*?)"}', text)
-        return r[0] if r else ""
-
-    def _find_html_dateTime(self, text):
-        """
-        find dateTime in html
-        """
-        r = findall('<time dateTime="(.*?)">', text)
-        if r:
-            return r
-        return []
-
-    def _find_date_in_response(self, r):
-        date_list = []
-        for item in r.json()["data"]["userProfile"]["feeds"]["nodes"]:
-            date_list.append(item["createdAt"])
-        return date_list
-
     def _get_first_last_id(self):
         """
         get first last id for first post request
@@ -106,23 +75,57 @@ class JikeLoader(BaseLoader):
             JIKE_PERSON_URL.format(user_id=self.user_id), headers=self.headers
         )
         if not r.ok:
-            raise LoadError(
-                "Can not get bilibili history data, please check your cookie"
-            )
+            raise LoadError("Can not get first last id, please check your cookie")
         else:
-            return self._find_html_last_id(r.text), self._find_html_dateTime(r.text)
+            return find_last_id_in_html(r.text), find_dateTime_in_html(r.text)
 
     def _request_post_data(self, last_id="", resp_date_list=[]):
         """
         request next page posts
         """
-        # req_data_temp = '{"operationName":"UserFeeds","variables":{"username":"","loadMoreKey":{"lastId":""}},"query":"query UserFeeds($username: String!, $loadMoreKey: JSON) {\n  userProfile(username: $username) {\n    username\n    feeds(loadMoreKey: $loadMoreKey) {\n      ...BasicFeedItem\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment BasicFeedItem on FeedsConnection {\n  pageInfo {\n    loadMoreKey\n    hasNextPage\n    __typename\n  }\n  nodes {\n    ... on ReadSplitBar {\n      id\n      type\n      text\n      __typename\n    }\n    ... on MessageEssential {\n      ...FeedMessageFragment\n      __typename\n    }\n    ... on UserAction {\n      id\n      type\n      action\n      actionTime\n      ... on UserFollowAction {\n        users {\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          __typename\n        }\n        allTargetUsers {\n          ...TinyUserFragment\n          following\n          statsCount {\n            followedCount\n            __typename\n          }\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          __typename\n        }\n        __typename\n      }\n      ... on UserRespectAction {\n        users {\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          __typename\n        }\n        targetUsers {\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          __typename\n        }\n        content\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment FeedMessageFragment on MessageEssential {\n  ...EssentialFragment\n  ... on OriginalPost {\n    ...LikeableFragment\n    ...CommentableFragment\n    ...RootMessageFragment\n    ...UserPostFragment\n    ...MessageInfoFragment\n    pinned {\n      personalUpdate\n      __typename\n    }\n    __typename\n  }\n  ... on Repost {\n    ...LikeableFragment\n    ...CommentableFragment\n    ...UserPostFragment\n    ...RepostFragment\n    pinned {\n      personalUpdate\n      __typename\n    }\n    __typename\n  }\n  ... on Question {\n    ...UserPostFragment\n    __typename\n  }\n  ... on OfficialMessage {\n    ...LikeableFragment\n    ...CommentableFragment\n    ...MessageInfoFragment\n    ...RootMessageFragment\n    __typename\n  }\n  __typename\n}\n\nfragment EssentialFragment on MessageEssential {\n  id\n  type\n  content\n  shareCount\n  repostCount\n  createdAt\n  collected\n  pictures {\n    format\n    watermarkPicUrl\n    picUrl\n    thumbnailUrl\n    smallPicUrl\n    width\n    height\n    __typename\n  }\n  urlsInText {\n    url\n    originalUrl\n    title\n    __typename\n  }\n  __typename\n}\n\nfragment LikeableFragment on LikeableMessage {\n  liked\n  likeCount\n  __typename\n}\n\nfragment CommentableFragment on CommentableMessage {\n  commentCount\n  __typename\n}\n\nfragment RootMessageFragment on RootMessage {\n  topic {\n    id\n    content\n    __typename\n  }\n  __typename\n}\n\nfragment UserPostFragment on MessageUserPost {\n  readTrackInfo\n  user {\n    ...TinyUserFragment\n    __typename\n  }\n  __typename\n}\n\nfragment TinyUserFragment on UserInfo {\n  avatarImage {\n    thumbnailUrl\n    smallPicUrl\n    picUrl\n    __typename\n  }\n  username\n  screenName\n  briefIntro\n  __typename\n}\n\nfragment MessageInfoFragment on MessageInfo {\n  video {\n    title\n    type\n    image {\n      picUrl\n      __typename\n    }\n    __typename\n  }\n  linkInfo {\n    originalLinkUrl\n    linkUrl\n    title\n    pictureUrl\n    linkIcon\n    audio {\n      title\n      type\n      image {\n        thumbnailUrl\n        picUrl\n        __typename\n      }\n      author\n      __typename\n    }\n    video {\n      title\n      type\n      image {\n        picUrl\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment RepostFragment on Repost {\n  target {\n    ...RepostTargetFragment\n    __typename\n  }\n  targetType\n  __typename\n}\n\nfragment RepostTargetFragment on RepostTarget {\n  ... on OriginalPost {\n    id\n    type\n    content\n    pictures {\n      thumbnailUrl\n      __typename\n    }\n    topic {\n      id\n      content\n      __typename\n    }\n    user {\n      ...TinyUserFragment\n      __typename\n    }\n    __typename\n  }\n  ... on Repost {\n    id\n    type\n    content\n    pictures {\n      thumbnailUrl\n      __typename\n    }\n    user {\n      ...TinyUserFragment\n      __typename\n    }\n    __typename\n  }\n  ... on Question {\n    id\n    type\n    content\n    pictures {\n      thumbnailUrl\n      __typename\n    }\n    user {\n      ...TinyUserFragment\n      __typename\n    }\n    __typename\n  }\n  ... on Answer {\n    id\n    type\n    content\n    pictures {\n      thumbnailUrl\n      __typename\n    }\n    user {\n      ...TinyUserFragment\n      __typename\n    }\n    __typename\n  }\n  ... on OfficialMessage {\n    id\n    type\n    content\n    pictures {\n      thumbnailUrl\n      __typename\n    }\n    __typename\n  }\n  ... on DeletedRepostTarget {\n    status\n    __typename\n  }\n  __typename\n}\n"}'
-        # fix json parse error
-        # req_data_temp = req_data_temp.replace('\n', "")
         payload_data = {
             "operationName": "UserFeeds",
             "variables": {"username": "", "loadMoreKey": {"lastId": ""}},
-            "query": "query UserFeeds($username: String!, $loadMoreKey: JSON) {\n  userProfile(username: $username) {\n    username\n    feeds(loadMoreKey: $loadMoreKey) {\n      ...BasicFeedItem\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment BasicFeedItem on FeedsConnection {\n  pageInfo {\n    loadMoreKey\n    hasNextPage\n    __typename\n  }\n  nodes {\n    ... on ReadSplitBar {\n      id\n      type\n      text\n      __typename\n    }\n    ... on MessageEssential {\n      ...FeedMessageFragment\n      __typename\n    }\n    ... on UserAction {\n      id\n      type\n      action\n      actionTime\n      ... on UserFollowAction {\n        users {\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          __typename\n        }\n        allTargetUsers {\n          ...TinyUserFragment\n          following\n          statsCount {\n            followedCount\n            __typename\n          }\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          __typename\n        }\n        __typename\n      }\n      ... on UserRespectAction {\n        users {\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          __typename\n        }\n        targetUsers {\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          ...TinyUserFragment\n          __typename\n        }\n        content\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment FeedMessageFragment on MessageEssential {\n  ...EssentialFragment\n  ... on OriginalPost {\n    ...LikeableFragment\n    ...CommentableFragment\n    ...RootMessageFragment\n    ...UserPostFragment\n    ...MessageInfoFragment\n    pinned {\n      personalUpdate\n      __typename\n    }\n    __typename\n  }\n  ... on Repost {\n    ...LikeableFragment\n    ...CommentableFragment\n    ...UserPostFragment\n    ...RepostFragment\n    pinned {\n      personalUpdate\n      __typename\n    }\n    __typename\n  }\n  ... on Question {\n    ...UserPostFragment\n    __typename\n  }\n  ... on OfficialMessage {\n    ...LikeableFragment\n    ...CommentableFragment\n    ...MessageInfoFragment\n    ...RootMessageFragment\n    __typename\n  }\n  __typename\n}\n\nfragment EssentialFragment on MessageEssential {\n  id\n  type\n  content\n  shareCount\n  repostCount\n  createdAt\n  collected\n  pictures {\n    format\n    watermarkPicUrl\n    picUrl\n    thumbnailUrl\n    smallPicUrl\n    width\n    height\n    __typename\n  }\n  urlsInText {\n    url\n    originalUrl\n    title\n    __typename\n  }\n  __typename\n}\n\nfragment LikeableFragment on LikeableMessage {\n  liked\n  likeCount\n  __typename\n}\n\nfragment CommentableFragment on CommentableMessage {\n  commentCount\n  __typename\n}\n\nfragment RootMessageFragment on RootMessage {\n  topic {\n    id\n    content\n    __typename\n  }\n  __typename\n}\n\nfragment UserPostFragment on MessageUserPost {\n  readTrackInfo\n  user {\n    ...TinyUserFragment\n    __typename\n  }\n  __typename\n}\n\nfragment TinyUserFragment on UserInfo {\n  avatarImage {\n    thumbnailUrl\n    smallPicUrl\n    picUrl\n    __typename\n  }\n  username\n  screenName\n  briefIntro\n  __typename\n}\n\nfragment MessageInfoFragment on MessageInfo {\n  video {\n    title\n    type\n    image {\n      picUrl\n      __typename\n    }\n    __typename\n  }\n  linkInfo {\n    originalLinkUrl\n    linkUrl\n    title\n    pictureUrl\n    linkIcon\n    audio {\n      title\n      type\n      image {\n        thumbnailUrl\n        picUrl\n        __typename\n      }\n      author\n      __typename\n    }\n    video {\n      title\n      type\n      image {\n        picUrl\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment RepostFragment on Repost {\n  target {\n    ...RepostTargetFragment\n    __typename\n  }\n  targetType\n  __typename\n}\n\nfragment RepostTargetFragment on RepostTarget {\n  ... on OriginalPost {\n    id\n    type\n    content\n    pictures {\n      thumbnailUrl\n      __typename\n    }\n    topic {\n      id\n      content\n      __typename\n    }\n    user {\n      ...TinyUserFragment\n      __typename\n    }\n    __typename\n  }\n  ... on Repost {\n    id\n    type\n    content\n    pictures {\n      thumbnailUrl\n      __typename\n    }\n    user {\n      ...TinyUserFragment\n      __typename\n    }\n    __typename\n  }\n  ... on Question {\n    id\n    type\n    content\n    pictures {\n      thumbnailUrl\n      __typename\n    }\n    user {\n      ...TinyUserFragment\n      __typename\n    }\n    __typename\n  }\n  ... on Answer {\n    id\n    type\n    content\n    pictures {\n      thumbnailUrl\n      __typename\n    }\n    user {\n      ...TinyUserFragment\n      __typename\n    }\n    __typename\n  }\n  ... on OfficialMessage {\n    id\n    type\n    content\n    pictures {\n      thumbnailUrl\n      __typename\n    }\n    __typename\n  }\n  ... on DeletedRepostTarget {\n    status\n    __typename\n  }\n  __typename\n}\n",
+            "query": """
+            query UserFeeds($username: String!, $loadMoreKey: JSON) {
+                userProfile(username: $username) {
+                    username
+                    feeds(loadMoreKey: $loadMoreKey) {
+                    ...BasicFeedItem
+                    __typename
+                    }
+                    __typename
+                }
+            }
+
+                fragment BasicFeedItem on FeedsConnection {
+                pageInfo {
+                    loadMoreKey
+                    hasNextPage
+                    __typename
+                }
+                nodes {
+                    ... on MessageEssential {
+                    ...FeedMessageFragment
+                    __typename
+                    }
+                    __typename
+                }
+                __typename
+                }
+
+                fragment FeedMessageFragment on MessageEssential {
+                ...EssentialFragment
+                __typename
+                }
+
+                fragment EssentialFragment on MessageEssential {
+                id
+                type
+                createdAt
+                __typename
+                }
+            """,
         }
         # set lastId
         payload_data["variables"]["loadMoreKey"]["lastId"] = last_id
@@ -131,20 +134,18 @@ class JikeLoader(BaseLoader):
         r = self.session.post(
             JIKE_GRAPHQL_URL, headers=self.headers, data=json.dumps(payload_data)
         )
-        resp_dates = self._find_date_in_response(r)
-        next_last_id = self._get_response_last_id(r.text)
+        resp_dates = find_date_in_response(r)
+        next_last_id = find_last_id_in_response(r.text)
         resp_date_list.extend(resp_dates)
         if self._check_if_stop(resp_dates) or next_last_id == "":
             return resp_date_list
-        time.sleep(random.randint(1, 5))
         self._request_post_data(next_last_id, resp_date_list)
 
     def _check_if_stop(self, list_of_dates):
-        this_year = self.from_year
         """
         stop when content other year data
         """
-        r_date = compile(str(this_year) + ".*?")
+        r_date = compile(str(self.from_year) + ".*?")
         filter_list = list(filter(r_date.match, list_of_dates))
         return len(filter_list) != len(list_of_dates)
 
@@ -176,4 +177,5 @@ class JikeLoader(BaseLoader):
         self.session.cookies = self._parse_jike_cookie()
         self.make_track_dict()
         self.make_special_number()
+        print("Thanks for being addicted to jike")
         return self.number_by_date_dict, self.year_list
