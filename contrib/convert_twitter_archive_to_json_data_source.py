da822947eec4ed256e00a1da5d8cada1d0d75494
Zhuoyun Wei <wzyboy@wzyboy.org>
2022-02-16 22:38:39 -0800

    lint

--- contrib/convert_twitter_archive_to_json_data_source.py
+++ contrib/convert_twitter_archive_to_json_data_source.py
@@ -15,43 +15,49 @@ More on your Twitter Archive: https://help.twitter.com/en/managing-your-account/
 More on GitHubPoster project: https://github.com/yihong0618/GitHubPoster
 """
 
-import json
 import argparse
-from datetime import datetime
+import json
 from collections import Counter
-
+from datetime import datetime
 
 # Wed Jul 08 15:04:01 +0000 2009
-TIMESTAMP_FMT = '%a %b %d %H:%M:%S %z %Y'
+TIMESTAMP_FMT = "%a %b %d %H:%M:%S %z %Y"
 
 
 def convert(input_file, output_file):
 
-    with open(input_file, 'r') as f:
+    with open(input_file, "r") as f:
         content = f.read()
 
     # Read JSON payload from the JavaScript file
-    preamble = 'window.YTD.tweet.part0 = '
-    content = content[len(preamble):]
+    preamble = "window.YTD.tweet.part0 = "
+    content = content[len(preamble) :]
 
     counter = Counter()
     tweets = json.loads(content)
     for tweet in tweets:
-        date = str(datetime.strptime(tweet['tweet']['created_at'], TIMESTAMP_FMT).date())
+        date = str(
+            datetime.strptime(tweet["tweet"]["created_at"], TIMESTAMP_FMT).date()
+        )
         counter[date] += 1
 
-    with open(output_file, 'w') as f:
+    with open(output_file, "w") as f:
         json.dump(counter, f, sort_keys=True, indent=2)
 
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('input_file', help='tweet.js file extracted from YourTwitterArchive.zip/data')
-    ap.add_argument('output_file', help='path to output file, which can be fed to GitHubPoster JSON loader')
+    ap.add_argument(
+        "input_file", help="tweet.js file extracted from YourTwitterArchive.zip/data"
+    )
+    ap.add_argument(
+        "output_file",
+        help="path to output file, which can be fed to GitHubPoster JSON loader",
+    )
 
     args = ap.parse_args()
     convert(args.input_file, args.output_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
