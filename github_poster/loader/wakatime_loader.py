b8ebcce8bc17465c842e7a49ac6587827d6c5ffa
BarryYangi <2059484047@qq.com>
2023-08-11 20:14:27 +0800

    fix: wakatime_loader

--- github_poster/loader/wakatime_loader.py
+++ github_poster/loader/wakatime_loader.py
@@ -1,10 +1,12 @@
 import time
-
 import requests
+import json
+import os
+from collections import defaultdict
+from datetime import datetime, timedelta
 
-from github_poster.loader.base_loader import BaseLoader
+from github_poster.loader.base_loader import BaseLoader, LoadError
 from github_poster.loader.config import WAKATIME_SUMMARY_URL
-
 
 class WakaTimeLoader(BaseLoader):
     track_color = "#9BE9A8"
@@ -13,6 +15,8 @@ class WakaTimeLoader(BaseLoader):
     def __init__(self, from_year, to_year, _type, **kwargs):
         super().__init__(from_year, to_year, _type)
         self.wakatime_key = kwargs.get("wakatime_key", "")
+        self.wakatime_file = kwargs.get("wakatime_history_file", os.path.join("IN_FOLDER", "wakatime-history.json"))
+        self._parse_wakatime_history()
 
     @classmethod
     def add_loader_arguments(cls, parser, optional):
@@ -21,34 +25,55 @@ class WakaTimeLoader(BaseLoader):
             dest="wakatime_key",
             type=str,
             required=optional,
-            help="your wakatime api key here, "
-            "more info: https://wakatime.com/settings/api-key",
+            help="your wakatime api key here, more info: https://wakatime.com/settings/api-key",
+        )
+        parser.add_argument(
+            "--wakatime_history_file",
+            dest="wakatime_history_file",
+            type=str,
+            default=os.path.join("IN_FOLDER", "wakatime-history.json"),
+            help="Wakatime history file path",
         )
 
+    def _parse_wakatime_history(self):
+        if os.path.exists(self.wakatime_file):
+            with open(self.wakatime_file, "r") as f:
+                self.number_by_date_dict = json.load(f)
+
+    def _writeback_wakatime_history(self):
+        with open(self.wakatime_file, "w") as f:
+            json.dump(self.number_by_date_dict, f, sort_keys=True)
+
     def get_api_data(self):
-        for year in range(self.from_year, self.to_year + 1):
-            r = requests.get(
-                WAKATIME_SUMMARY_URL.format(
-                    wakatime_key=self.wakatime_key,
-                    from_year="{}-01-01".format(year),
-                    to_year="{}-12-31".format(year),
-                )
+        if self.from_year != self.to_year:  # 年份间隔超过一年
+            start_date = f"{self.from_year}-01-01"
+            end_date = f"{self.to_year}-12-31"
+        else:  # 保持原有的最近两周逻辑
+            start_date = (datetime.now() - timedelta(days=14)).strftime('%Y-%m-%d')
+            end_date = datetime.now().strftime('%Y-%m-%d')
+        
+        r = requests.get(
+            WAKATIME_SUMMARY_URL.format(
+                wakatime_key=self.wakatime_key,
+                from_year=start_date,
+                to_year=end_date,
             )
-            if not r.ok:
-                yield []
-            data = r.json()
-            yield from data["data"]
-            # spider rule
-            time.sleep(1)
+        )
+        if not r.ok:
+            return []
+        data = r.json()
+        # spider rule
+        time.sleep(1)
+        return data.get("data", [])
 
     def make_track_dict(self):
         data_list = self.get_api_data()
         for d in data_list:
             if d:
                 date = d["range"]["date"]
-                self.number_by_date_dict[date] += int(
-                    d["grand_total"]["total_seconds"] / 60.0
-                )
+                if date not in self.number_by_date_dict:
+                    self.number_by_date_dict[date] = int(d["grand_total"]["total_seconds"] / 60.0)
+        self._writeback_wakatime_history()
         for _, v in self.number_by_date_dict.items():
             self.number_list.append(v)
 
